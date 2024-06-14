758417c7c8b90ea56104ccc9081b901c6ee310f1
tsabot <tsabot.pro@gmail.com>
2024-06-14 16:29:54 +0200

    chore(filter): extracted heavy mocks from tests

--- src/filter/logic/reducer.test.js
+++ src/filter/logic/reducer.test.js
@@ -5,6 +5,12 @@ const {
   reduceCountryByAnimals,
 } = require("./reducer");
 
+const {
+  mockedAnimals,
+  mockedCountries,
+  mockedPeople,
+} = require("../mock/mockData");
+
 describe("doesNameIncludes", () => {
   it("should return true if string is included", () => {
     const bool = doesNameIncludes("charle", "char");
@@ -17,15 +23,6 @@ describe("doesNameIncludes", () => {
     expect(bool).toEqual(false);
   });
 });
-
-const mockedAnimals = [
-  { name: "Anoa" },
-  { name: "Duck" },
-  { name: "Narwhal" },
-  { name: "Badger" },
-  { name: "Cobra" },
-  { name: "Crow" },
-];
 
 describe("reduce animals", () => {
   it("should return animals if string is included in their name", () => {
@@ -45,69 +42,6 @@ describe("reduce animals", () => {
   });
 });
 
-const mockedPeople = [
-  {
-    name: "Winifred Graham",
-    animals: [
-      { name: "Anoa" },
-      { name: "Duck" },
-      { name: "Narwhal" },
-      { name: "Badger" },
-      { name: "Cobra" },
-      { name: "Crow" },
-    ],
-  },
-  {
-    name: "Blanche Viciani",
-    animals: [
-      { name: "Barbet" },
-      { name: "Rhea" },
-      { name: "Snakes" },
-      { name: "Antelope" },
-      { name: "Echidna" },
-      { name: "Crow" },
-      { name: "Guinea Fowl" },
-      { name: "Deer Mouse" },
-    ],
-  },
-  {
-    name: "Philip Murray",
-    animals: [
-      { name: "Sand Dollar" },
-      { name: "Buzzard" },
-      { name: "Elephant" },
-      { name: "Xenops" },
-      { name: "Dormouse" },
-      { name: "Anchovy" },
-      { name: "Dinosaur" },
-    ],
-  },
-  {
-    name: "Bobby Ristori",
-    animals: [
-      { name: "Kowari" },
-      { name: "Caecilian" },
-      { name: "Common Genet" },
-      { name: "Chipmunk" },
-      { name: "Aardwolf" },
-      { name: "Przewalski's Horse" },
-      { name: "Badger" },
-      { name: "Sand Cat" },
-      { name: "Linne's Two-toed Sloth" },
-    ],
-  },
-  {
-    name: "Louise Pinzauti",
-    animals: [
-      { name: "Manta Ray" },
-      { name: "Nubian Ibex" },
-      { name: "Warbler" },
-      { name: "Duck" },
-      { name: "Mice" },
-    ],
-  },
-];
-
 describe("reducePeopleByAnimals", () => {
   it("should return people if animal name is included in their animals", () => {
     const filteredAnimals = reducePeopleByAnimals(mockedPeople, "uc");
@@ -124,170 +58,6 @@ describe("reducePeopleByAnimals", () => {
     ]);
   });
 });
-
-const mockedCountries = [
-  {
-    name: "Dillauti",
-    people: [
-      {
-        name: "Winifred Graham",
-        animals: [
-          { name: "Anoa" },
-          { name: "Duck" },
-          { name: "Narwhal" },
-          { name: "Badger" },
-          { name: "Cobra" },
-          { name: "Crow" },
-        ],
-      },
-      {
-        name: "Blanche Viciani",
-        animals: [
-          { name: "Barbet" },
-          { name: "Rhea" },
-          { name: "Snakes" },
-          { name: "Antelope" },
-          { name: "Echidna" },
-          { name: "Crow" },
-          { name: "Guinea Fowl" },
-          { name: "Deer Mouse" },
-        ],
-      },
-      {
-        name: "Philip Murray",
-        animals: [
-          { name: "Sand Dollar" },
-          { name: "Buzzard" },
-          { name: "Elephant" },
-          { name: "Xenops" },
-          { name: "Dormouse" },
-          { name: "Anchovy" },
-          { name: "Dinosaur" },
-        ],
-      },
-      {
-        name: "Bobby Ristori",
-        animals: [
-          { name: "Kowari" },
-          { name: "Caecilian" },
-          { name: "Common Genet" },
-          { name: "Chipmunk" },
-          { name: "Aardwolf" },
-          { name: "Przewalski's Horse" },
-          { name: "Badger" },
-          { name: "Sand Cat" },
-          { name: "Linne's Two-toed Sloth" },
-        ],
-      },
-      {
-        name: "Louise Pinzauti",
-        animals: [
-          { name: "Manta Ray" },
-          { name: "Nubian Ibex" },
-          { name: "Warbler" },
-          { name: "Duck" },
-          { name: "Mice" },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Tohabdal",
-    people: [
-      {
-        name: "Effie Houghton",
-        animals: [
-          { name: "Zebra" },
-          { name: "Ring-tailed Lemur" },
-          { name: "Fly" },
-          { name: "Blue Iguana" },
-          { name: "Emu" },
-          { name: "African Wild Ass" },
-          { name: "Numbat" },
-        ],
-      },
-      {
-        name: "Essie Bennett",
-        animals: [
-          { name: "Aldabra Tortoise" },
-          { name: "Patagonian Toothfish" },
-          { name: "Giant Panda" },
-          { name: "Goat" },
-          { name: "Quahog" },
-          { name: "Collared Lemur" },
-          { name: "Aldabra Tortoise" },
-        ],
-      },
-      {
-        name: "Owen Bongini",
-        animals: [
-          { name: "Zebrashark" },
-          { name: "Dogs" },
-          { name: "Mouse" },
-          { name: "Numbat" },
-          { name: "African Wild Dog" },
-        ],
-      },
-      {
-        name: "Alexander Fleury",
-        animals: [
-          { name: "Gelada" },
-          { name: "Rattlesnake" },
-          { name: "Rabbit" },
-          { name: "Aardvark" },
-          { name: "Duck" },
-          { name: "Courser" },
-          { name: "Woodpecker" },
-        ],
-      },
-      {
-        name: "Curtis Fuchs",
-        animals: [
-          { name: "Squirrel" },
-          { name: "Falcon" },
-          { name: "Cat" },
-          { name: "Lobe Coral" },
-          { name: "Camel" },
-          { name: "Broadclub Cuttlefish" },
-        ],
-      },
-      {
-        name: "Maud Lorenzo",
-        animals: [
-          { name: "Bush Dog" },
-          { name: "Sea Urchin" },
-          { name: "Gayal" },
-          { name: "Tortoise" },
-          { name: "Meerkat" },
-          { name: "Lion" },
-          { name: "Gecko" },
-        ],
-      },
-      {
-        name: "Linnie Lamb",
-        animals: [
-          { name: "Burro" },
-          { name: "African Wild Dog" },
-          { name: "Slender Snipe Eel" },
-          { name: "Red Panda" },
-          { name: "Baby Doll Sheep" },
-          { name: "California Sea Lion" },
-          { name: "Rabbits" },
-        ],
-      },
-      {
-        name: "Randall Benoît",
-        animals: [
-          { name: "Chameleons" },
-          { name: "Bee-eater" },
-          { name: "King Vulture" },
-          { name: "Giant Isopod" },
-          { name: "Sand Cat" },
-        ],
-      },
-    ],
-  },
-];
 
 describe("reduceCountryByAnimals", () => {
   it("should return country if animal name is included in at least one of their animals", () => {
