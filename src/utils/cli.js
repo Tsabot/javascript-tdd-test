f8c3b721c464a92bc83b9fb0bc49a4301ee50c98
tsabot <tsabot.pro@gmail.com>
2024-06-14 16:44:35 +0200

    feat(filter): green cli get args

--- src/utils/cli.js
+++ src/utils/cli.js
@@ -1,5 +1,11 @@
 const getArgumentFromCli = () => {
-  return "";
+  const cliArg = process.argv[2];
+
+  if (cliArg.includes("filter")) {
+    return cliArg.slice(9);
+  }
+
+  return cliArg;
 };
 
 module.exports = {
--- src/utils/cli.test.js
+++ src/utils/cli.test.js
@@ -1,9 +1,11 @@
 const { getArgumentFromCli } = require("./cli");
 
+jest.replaceProperty(process, "argv", ["node", "path", "--filter=ry"]);
+
 describe("getArgumentFromCli", () => {
   it("should get argument from cli", () => {
     const string = getArgumentFromCli();
 
-    expect(string).toEqual("args");
+    expect(string).toEqual("ry");
   });
 });
