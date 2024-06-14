a86c891c984e88ee31a7e08400bb30349ad6f0be
tsabot <tsabot.pro@gmail.com>
2024-06-14 17:00:57 +0200

    feat(filter): red should log arg

--- src/filter/intex.test.js
+++ src/filter/intex.test.js
@@ -12,4 +12,11 @@ describe("executeFilterOnData", () => {
 
     expect(console.log).toHaveBeenCalled();
   });
+  it("should log provided cli argument", () => {
+    jest.replaceProperty(process, "argv", ["node", "path", "--filter=ry"]);
+
+    executeFilterOnData();
+
+    expect(console.log).toHaveBeenCalledWith("ry");
+  });
 });
