d0cbfcce628360ecb73ef8fe54716b91de75e93e
tsabot <tsabot.pro@gmail.com>
2024-06-14 19:58:49 +0200

    chore(count): removed useless complexity on count properties

--- src/count/logic/count.test.js
+++ src/count/logic/count.test.js
@@ -18,19 +18,6 @@ describe("concatCountInName", () => {
   });
 });
 
-describe("count number of animal in people", () => {
-  it("should return the number of animals of someone", () => {
-    const count = countPropertyOfObject(mockedPerson, "animals");
-
-    expect(count).toEqual(6);
-  });
-  it("should return the number of people in a country", () => {
-    const count = countPropertyOfObject(mockedCountry, "people");
-
-    expect(count).toEqual(5);
-  });
-});
-
 describe("count and format name", () => {
   it("should count the number of animals and add it to it's name", () => {
     const newPersonName = countAndFormatName(mockedPerson, "animals");
